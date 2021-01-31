7a8e2b9adc30950829aadc015def43c6a00c6d65
Glitch (mixed-nostalgic-process) <none>
2021-01-31 07:28:47 +0000

    🚣💝 Checkpoint ./public/index.html:27683896/272 ./public/script.js:27683896/981

--- public/script.js
+++ public/script.js
@@ -1,7 +1,31 @@
 /*
   Project:   Hands-On Project 1-2
-	Author:    
-	Date:      
+	Author:    Guillermo Molina Matus
+	Date:      1/31/21
 	Purpose:   To illustrate variable initialization and use in a method
 */
 
+//initialize variables for service names and speeds
+
+var service1Name = "Basic";
+var service2Name = "Express";
+var service3Name = "Extreme";
+var service4Name = "Ultimate";
+
+var service1Speed =  5;
+var service2Speed =  25;
+var service3Speed =  50;
+var service4Speed =  150;
+
+
+
+/*
+creating the table
+*/
+
+document.write("<tr><td>"); //Creates a new table row "tr" and first table cell "td"
+document.write(service1Name); //Writes out the value stored in service1Name
+document.write("</td>"); //closes table cell
+document.write("<td>"); //creates new table cell 
+document.write(service1Speed); //displays value for service 1
+document.write("</td></tr>"); //ends the table row
